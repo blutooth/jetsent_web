cdb9d93ae09bb153a9e2abe1b15faf15783ca32c
blutooth <maximilian@live.co.uk>
2019-05-19 03:09:55 +0100

    added javascripts

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -20,16 +20,3 @@ environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 
 
-environment.loaders.append('expose', {
-    test: require.resolve('jquery'),
-    use: [{
-        loader: 'expose-loader',
-        options: '$'
-    }, {
-        loader: 'expose-loader',
-        options: 'jQuery',
-
-    },{
-        loader: 'expose-loader',
-        options: 'jquery'}]
-})
